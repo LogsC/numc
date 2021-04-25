b3497dcc07465ae747063f2b6cc93b50329430c5
LogsC <logan.c.chiu@berkeley.edu>
2021-04-25 12:10:40 -0700

    test add cases

--- tests/unittests/unittests.py
+++ tests/unittests/unittests.py
@@ -19,11 +19,19 @@ class TestAdd(TestCase):
 
     def test_medium_add(self):
         # TODO: YOUR CODE HERE
-        pass
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(10, 10, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(10, 10, seed=1)
+        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
+        self.assertTrue(is_correct)
+        print_speedup(speed_up)
 
     def test_large_add(self):
         # TODO: YOUR CODE HERE
-        pass
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(100, 100, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(100, 100, seed=1)
+        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
+        self.assertTrue(is_correct)
+        print_speedup(speed_up)
 
 class TestSub(TestCase):
     def test_small_sub(self):
